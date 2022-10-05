47feaa64e2af374f5de0e913af46451cdf727073
Eduardo Pereira <eduardojpereira@gmail.com>
2022-10-05 01:21:22 +0000

    Alteracoes no preço do dogao

--- dogao.sh
+++ dogao.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Dog completo 15,90"
-echo "Dog Duplo 19,40"
+echo "Dog completo 14,90"
+echo "Dog Duplo 21,00"
 echo "Dog Duplo com adicional de Bacon 32,00"
-echo "Dog Simples 10,00"
+echo "Dog Simples 9,00"
